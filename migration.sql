8c003dcf416bf035131820369256ad39afe72ac9
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-08-28 15:40:21 -0500

    added thing and exercise table

--- migration.sql
+++ migration.sql
@@ -5,6 +5,10 @@ USE dad_db;
 DROP TABLE IF EXISTS dads;
 DROP TABLE IF EXISTS bbq;
 DROP TABLE IF EXISTS new_balance;
+DROP TABLE IF EXISTS new_dads;
+DROP TABLE IF EXISTS thingamajig;
+DROP TABLE IF EXISTS exercise;
+
 
 CREATE TABLE dads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -34,12 +38,51 @@ CREATE TABLE bbq (
 
 CREATE TABLE new_balance (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    dads_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
+    dads_id INT UNSIGNED NOT NULL,
+    dad_username VARCHAR(240) NOT NULL,
     title VARCHAR(240) NOT NULL,
     time DATETIME NOT NULL,
     location VARCHAR(240) NOT NULL,
     dadscription VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (dads_id) REFERENCES dads(id)
+    FOREIGN KEY (dads_id) REFERENCES dads(id),
+    FOREIGN KEY (dad_username) REFERENCES dads (username)
 );
+
+CREATE TABLE new_dads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    dads_id INT UNSIGNED NOT NULL,
+    dad_username VARCHAR(240) NOT NULL,
+    title VARCHAR(240) NOT NULL,
+    dadscription VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dads_id) REFERENCES dads (id),
+    FOREIGN KEY (dad_username) REFERENCES dads (username)
+
+);
+
+CREATE TABLE thingamajig (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    dads_id INT UNSIGNED NOT NULL,
+    dad_username VARCHAR(240) NOT NULL,
+    title VARCHAR(240) NOT NULL,
+    dadscription VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dads_id) REFERENCES dads (id),
+    FOREIGN KEY (dad_username) REFERENCES dads (username)
+
+);
+
+CREATE TABLE exercise (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    dads_id INT UNSIGNED NOT NULL,
+    dad_username VARCHAR(240) NOT NULL,
+    title VARCHAR(240) NOT NULL,
+    dadscription VARCHAR(255) NOT NULL,
+    locationn VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dads_id) REFERENCES dads (id),
+    FOREIGN KEY (dad_username) REFERENCES dads (username)
+
+);
+
