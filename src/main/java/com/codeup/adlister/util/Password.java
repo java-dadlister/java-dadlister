ad0294b6583539107ee7ce9409a0c6798933fe38
Graham Davis <davis.graham.satx@gmail.com>
2018-08-29 15:47:39 -0500

    fixed login/logout issues with password hashing

--- src/main/java/com/codeup/adlister/util/Password.java
+++ src/main/java/com/codeup/adlister/util/Password.java
@@ -3,7 +3,7 @@ package com.codeup.adlister.util;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class Password {
-    private static final int ROUNDS = 12;
+    private static final int ROUNDS = 10;
 
     public static String hash(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(ROUNDS));
@@ -11,5 +11,9 @@ public class Password {
 
     public static boolean check(String password, String hash) {
         return BCrypt.checkpw(password, hash);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Password.hash("codeup123"));
     }
 }
