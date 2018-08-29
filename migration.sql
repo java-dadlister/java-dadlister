c0125e356144ab20893e7c8687849d502dff0994
Graham Davis <davis.graham.satx@gmail.com>
2018-08-29 13:16:26 -0500

    added the  new balance and the meats category seeders

--- migration.sql
+++ migration.sql
@@ -12,33 +12,33 @@ DROP TABLE IF EXISTS exercise;
 
 CREATE TABLE dads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(240) NOT NULL,
-    last_name VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
-    username VARCHAR(240) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    favorite_joke VARCHAR(2083) NOT NULL,
-    bio VARCHAR(2083) NOT NULL,
-    img_url VARCHAR(2083) DEFAULT 'http://via.placeholder.com/300x300',
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    username VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    favorite_joke VARCHAR(2500) NOT NULL,
+    bio VARCHAR(2500) NOT NULL,
+    img_url VARCHAR(150) DEFAULT 'http://via.placeholder.com/300x300',
     PRIMARY KEY (id)
 );
 
 CREATE TABLE the_meats (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    dad_id INT UNSIGNED NOT NULL,
-    the_meat VARCHAR(240) NOT NULL,
-    time DATETIME NOT NULL,
-    location VARCHAR(240) NOT NULL,
-    dadscription VARCHAR(255) NOT NULL,
+    dads_id INT UNSIGNED NOT NULL,
+    the_meat VARCHAR(200) NOT NULL,
+    time VARCHAR(50) NOT NULL,
+    location VARCHAR(200) NOT NULL,
+    dadscription VARCHAR(2500) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (dad_id) REFERENCES dads (id)
+    FOREIGN KEY (dads_id) REFERENCES dads (id)
 );
 
 CREATE TABLE new_balance (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     dads_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    dadscription VARCHAR(255) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    dadscription VARCHAR(2500) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (dads_id) REFERENCES dads(id)
 );
@@ -46,8 +46,8 @@ CREATE TABLE new_balance (
 CREATE TABLE new_dads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     dads_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    dadscription VARCHAR(255) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    dadscription VARCHAR(2500) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (dads_id) REFERENCES dads (id)
 );
@@ -55,8 +55,8 @@ CREATE TABLE new_dads (
 CREATE TABLE thingamajig (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     dads_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    dadscription VARCHAR(255) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    dadscription VARCHAR(2500) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (dads_id) REFERENCES dads (id)
 );
@@ -64,8 +64,8 @@ CREATE TABLE thingamajig (
 CREATE TABLE exercise (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     dads_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    dadscription VARCHAR(255) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    dadscription VARCHAR(2500) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (dads_id) REFERENCES dads (id)
 );
