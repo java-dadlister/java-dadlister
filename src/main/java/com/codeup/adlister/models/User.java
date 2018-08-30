d8612e8c648028be8bbf6d206c3ba333e34a6864
Graham Davis <davis.graham.satx@gmail.com>
2018-08-30 10:35:24 -0500

    refactoring login pages

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -26,12 +26,27 @@ public class User {
         this.img_url = img_url;
     }
 
+    public User(int id, String email, String username, String password, String favorite_joke, String bio) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.favorite_joke = favorite_joke;
+        this.bio = bio;
+    }
+
     public User(String email, String username, String password, String favorite_joke, String bio) {
         this.email = email;
         this.username = username;
         this.password = password;
         this.favorite_joke = favorite_joke;
         this.bio = bio;
+    }
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
     public int getId() {
