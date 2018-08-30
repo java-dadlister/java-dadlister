b240fd2636af1cfe196e13c2b0e20845fc0e39e8
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-08-30 10:33:44 -0500

    added fav and bio

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -46,7 +46,7 @@ public class RegisterServlet extends HttpServlet {
             }
 
             // create and save a new user
-            User user = new User(username, email, password);
+            User user = new User(username, email, password, favorite_joke, bio );
             DaoFactory.getUsersDao().insert(user);
             response.sendRedirect("/login");
         }
