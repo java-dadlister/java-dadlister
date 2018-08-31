21fb356a19333dd0fa4319c00218378f5231b1a5
Graham Davis <davis.graham.satx@gmail.com>
2018-08-31 11:09:08 -0500

    added users delete option

--- src/main/java/dao/users/Users.java
+++ src/main/java/dao/users/Users.java
@@ -2,7 +2,14 @@ package dao.users;
 
 import models.User;
 
+import java.util.List;
+
 public interface Users {
     User findByUsername(String username);
     Long insert(User user);
+    List<User> all();
+    User find(String column, String value);
+    boolean delete(long id);
+    void save(User user);
+    long verifyEmailPass(String email, String pass);
 }
