5fea028f3d73c3adbd79af734f68505c8146a916
Graham Davis <davis.graham.satx@gmail.com>
2018-08-31 12:15:58 -0500

    changed the login and register servlets

--- src/main/java/dao/users/MySQLUsersDao.java
+++ src/main/java/dao/users/MySQLUsersDao.java
@@ -3,92 +3,207 @@ package dao.users;
 import com.mysql.cj.jdbc.Driver;
 import dao.Config;
 import models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
+import javax.xml.transform.Result;
 import java.sql.*;
-
-import dao.users.Users;
-import models.User;
-import com.mysql.cj.jdbc.Driver;
-
-import java.sql.*;
-
-    public class MySQLUsersDao implements Users {
-        private Connection connection;
-
-        public MySQLUsersDao(Config config) {
-            try {
-                DriverManager.registerDriver(new Driver());
-                connection = DriverManager.getConnection(
-                        config.getUrl(),
-                        config.getUser(),
-                        config.getPassword()
-                );
-            } catch (SQLException e) {
-                throw new RuntimeException("Error connecting to the database!", e);
-            }
-        }
+import java.util.ArrayList;
+import java.util.List;
 
 
-        @Override
-        public User findByUsername(String username) {
-            String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
-            try {
-                PreparedStatement stmt = connection.prepareStatement(query);
-                stmt.setString(1, username);
-                return extractUser(stmt.executeQuery());
-            } catch (SQLException e) {
-                throw new RuntimeException("Error finding a user by username", e);
-            }
-        }
+public class MySQLUsersDao implements Users {
 
-        @Override
-        public Long insert(User user) {
-            String query = "INSERT INTO users(first_name, last_name, email, username, password, favorite_joke, bio) VALUES (?, ?, ?, ?, ?, ?, ?)";
-            try {
-                PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-                System.out.println(user.getUsername());
-                stmt.setString(1, user.getFirst_name());
-                stmt.setString(2, user.getLast_name());
-                stmt.setString(3, user.getEmail());
-                stmt.setString(4, user.getUsername());
-                stmt.setString(5, user.getPassword());
-                stmt.setString(6, user.getFavorite_joke());
-                stmt.setString(7, user.getBio());
-                stmt.executeUpdate();
-                ResultSet rs = stmt.getGeneratedKeys();
-                rs.next();
-                return rs.getLong(1);
-            } catch (SQLException e) {
-                throw new RuntimeException("Error creating new user", e);
-            }
-        }
+    private Connection connection;
 
-        private User extractUser(ResultSet rs) throws SQLException {
-            if (! rs.next()) {
-                return null;
-            }
-            return new User(
-                    // these MUST match the order in the table
-                    rs.getInt("id"),
-                    rs.getString("first_name"),
-                    rs.getString("last_name"),
-                    rs.getString("email"),
-                    rs.getString("username"),
-                    rs.getString("password"),
-                    rs.getString("favorite_joke"),
-                    rs.getString("bio"),
-                    rs.getString("img_url")
+    public MySQLUsersDao(Config config) {
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
             );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
         }
     }
 
-//      this.id = id;
-//              this.first_name = first_name;
-//              this.last_name = last_name;
-//              this.email = email;
-//              this.username = username;
-//              this.password = password;
-//              this.favorite_joke = favorite_joke;
-//              this.bio = bio;
-//              this.img_url = img_url;
+
+    @Override
+    public List<User> all() {
+        String query = "SELECT * FROM users";
+
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet usersRs = stmt.executeQuery(query);
+            List<User> users = new ArrayList<>();
+
+            while (usersRs.next()) {
+
+                int id = usersRs.getInt("id");
+                String first_name = usersRs.getString("first_name");
+                String last_name = usersRs.getString("last_name");
+                String email = usersRs.getString("email");
+                String username = usersRs.getString("username");
+                String password = usersRs.getString("password");
+                String favorite_joke = usersRs.getString("favorite_joke");
+                String bio = usersRs.getString("bio");
+                String img_url = usersRs.getString("img_url");
+
+                User user = new User(id, first_name, last_name, email, username, password, favorite_joke, bio, img_url);
+
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error getting all users");
+        }
+    }
+
+    @Override
+    public User find(String column, String value) {
+
+        //attempt to validate proper column values and keep them variable
+        String columnValue;
+        if (column.equals("first_name") || column.equals("last_name") || column.equals("username") || column.equals("id") || column.equals("email")) {
+            columnValue = column;
+        } else {
+            throw new RuntimeException("Invalid column name!");
+        }
+
+        String query = "SELECT * FROM users WHERE " + columnValue + " = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, value);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                int id = rs.getInt("id");
+                String first_name = rs.getString("first_name");
+                String last_name = rs.getString("last_name");
+                String email = rs.getString("email");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String favorite_joke = rs.getString("favorite_joke");
+                String bio = rs.getString("bio");
+                String img_url = rs.getString("img_url");
+
+                return new User(id, first_name, last_name, email, username, password, favorite_joke, bio, img_url);
+
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error finding user");
+        }
+    }
+
+    @Override
+    public boolean delete(long id) {
+
+        String query = "DELETE FROM users WHERE id = ?";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setLong(1, id);
+            return ps.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Problem");
+            throw new RuntimeException("Error deleting user");
+        }
+
+    }
+
+    @Override
+    public void save(User user) {
+        if (user.getId() == 0) {
+            insert(user);
+        } else {
+            update(user);
+        }
+    }
+
+    public Long insert(User user) {
+        String query = "INSERT INTO users (first_name, last_name, email, username, password, favorite_joke, bio) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+//            System.out.println(user.getUsername());
+            stmt.setString(1, user.getFirst_name());
+            stmt.setString(2, user.getLast_name());
+            stmt.setString(3, user.getEmail());
+            stmt.setString(4, user.getUsername());
+            stmt.setString(5, user.getPassword());
+            stmt.setString(6, user.getFavorite_joke());
+            stmt.setString(7, user.getBio());
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            rs.next();
+
+            return rs.getLong(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error creating new user", e);
+        }
+    }
+
+    public boolean update(User user) {
+        String query = "UPDATE users SET first_name = ?, last_name = ?, email = ?, username = ?, password = ?, favorite_joke = ?, bio = ?, img_url = ?, id = ?";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+
+            ps.setString(1, user.getFirst_name());
+            ps.setString(2, user.getLast_name());
+            ps.setString(3, user.getEmail());
+            ps.setString(4, user.getUsername());
+            ps.setString(5, user.getPassword());
+            ps.setString(6, user.getFavorite_joke());
+            ps.setString(7, user.getBio());
+            ps.setString(8, user.getImg_url());
+            ps.setLong(9, user.getId());
+
+            return ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error updating User");
+        }
+
+//        @Override
+//        public long verifyEmailPass (String email, String pass){
+//
+//            User user = find("email", email);
+//
+//            if (user == null) {
+//                return 0;
+//            } else {
+//                if (BCrypt.checkpw(pass, user.getPassword())) {
+//                    return user.getId();
+//                } else {
+//                    return 0;
+//                }
+//            }
+//        }
+    }
+}
+
+
+
+
+
+
+
+
 
--- src/main/java/dao/users/Users.java
+++ src/main/java/dao/users/Users.java
@@ -5,11 +5,11 @@ import models.User;
 import java.util.List;
 
 public interface Users {
-    User findByUsername(String username);
-    Long insert(User user);
+//    User findByUsername(String username);
+//    Long insert(User user);
     List<User> all();
     User find(String column, String value);
     boolean delete(long id);
     void save(User user);
-    long verifyEmailPass(String email, String pass);
+//    long verifyEmailPass(String email, String pass);
 }
